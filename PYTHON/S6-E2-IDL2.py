fa1160b6670a8e0268bbca90ea5a69e917b9b9bd
LAB_A_REAL <laba@continental.edu.pe>
2026-07-04 09:53:56 -0500

    Primer avance de HTML y CSS

--- PYTHON/S6-E2-IDL2.py
+++
@@ -1,8 +0,0 @@
-print()
-print("=== MAYOR DE EDAD ===")
-print(" AUTOR: YAHYR FLORES ")
-print()
-#
-EDAD=int(input("ESCRIBA LA EDAD:"))
-if EDAD >= 18:
-    print("MAYOR DE EDAD")
--- PYTHON/S7-E2-IDL2.py
+++
@@ -1,9 +0,0 @@
-print()
-print("=== DESCUENTO ===")
-print("AUTOR: Yahyr Flores ")
-print()
-#leer datos
-EDAD=int(input("Ingrese la edad: "))
-if (EDAD>18) and (EDAD<25):
-    print("Tienes un 10% De descuento")
-    
